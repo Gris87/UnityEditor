18bfec5fa3698efabd7a43233eed9715d7856570
Gris87 <Gris87@yandex.ru>
2014-12-25 00:36:21 +0300

    First try to kill popup menu in pressing outside

---
+++ Assets/Scripts/ui/ConfigurableRaycast.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+
+public class ConfigurableRaycast : MonoBehaviour, ICanvasRaycastFilter 
+{
+	public bool catchTouches = true;
+
+	public bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
+	{
+		return catchTouches;
+	}
+}
--- Assets/Scripts/ui/PopupMenuArea/PopupMenuAreaScript.cs
+++ Assets/Scripts/ui/PopupMenuArea/PopupMenuAreaScript.cs
@@ -1,12 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 
 
-public class PopupMenuAreaScript : MonoBehaviour
+public class PopupMenuAreaScript : MonoBehaviour, IPointerDownHandler
 {
 	public Sprite background;
 	public Sprite separator;
 	public Button itemButton;
 	public Button itemButtonDisabled;
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		Debug.Log("fghfdh");
+		eventData.Reset();
+	}
 }
